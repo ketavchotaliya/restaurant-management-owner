59b6c9dcca24ba30117fa80b11bdd181f00f048c
Ketav Chotaliya <chotaliyaketav@gmail.com>
2021-01-06 13:51:59 +0530

    Restaurant timing validation add

--- src/components/restaurant/restaurant.validation.ts
+++ src/components/restaurant/restaurant.validation.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import { createValidationResponse } from '../../utils/helper';
-import { isBoolean, isEmpty, isJSON, isNumber, isString } from '../../utils/validator';
+import { isBoolean, isEmpty, isJSON, isNumber, isString, matches } from '../../utils/validator';
+const TIME_VALIDATION_REGEX = /(\d{1,2})\:(\d{1,2})\:(\d{1,2})/;
 
 class RestaurantValidations {
   addRestaurant(req: Request, res: Response, next: NextFunction) {
@@ -30,14 +31,18 @@ class RestaurantValidations {
 
     if (isEmpty(open_time_1)) {
       errors.open_time_1 = res.__('RESTAURANT.open_time_1.required');
-    } else if (!isNumber(open_time_1)) {
-      errors.open_time_1 = res.__('RESTAURANT.open_time_1.valid');
+    } else if (!/^([0-1]?[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9])?$/.test(open_time_1)) {
+      errors.open_time_1 = res.__('RESTAURANT.open_time_1.invalid_time');
     }
 
     if (isEmpty(close_time_1)) {
       errors.close_time_1 = res.__('RESTAURANT.close_time_1.required');
-    } else if (!isNumber(close_time_1)) {
-      errors.close_time_1 = res.__('RESTAURANT.close_time_1.valid');
+    } else if (!/^([0-1]?[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9])?$/.test(close_time_1)) {
+      errors.close_time_1 = res.__('RESTAURANT.close_time_1.invalid_time');
+    } else if (
+      close_time_1.replace(TIME_VALIDATION_REGEX, '$1$2$3') <= open_time_1.replace(TIME_VALIDATION_REGEX, '$1$2$3')
+    ) {
+      errors.close_time_1 = res.__('RESTAURANT.close_time_1.less_then_start_time');
     }
 
     if (isEmpty(min_reservation_period)) {
@@ -46,12 +51,28 @@ class RestaurantValidations {
       errors.min_reservation_period = res.__('RESTAURANT.min_reservation_time.number');
     }
 
-    if (!isEmpty(open_time_2) && !isNumber(open_time_2)) {
-      errors.close_time_1 = res.__('RESTAURANT.open_time_1.valid');
+    if (!isEmpty(open_time_2)) {
+      if (!/^([0-1]?[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9])?$/.test(open_time_2)) {
+        errors.open_time_2 = res.__('RESTAURANT.open_time_1.invalid_time');
+      } else if (
+        open_time_2.replace(TIME_VALIDATION_REGEX, '$1$2$3') <= close_time_1.replace(TIME_VALIDATION_REGEX, '$1$2$3')
+      ) {
+        errors.close_time_1 = res.__('RESTAURANT.open_time_2.less_then_start_time');
+      }
+
+      if (isEmpty(close_time_2)) {
+        errors.close_time_2 = res.__('RESTAURANT.close_time_2.required');
+      }
     }
 
-    if (!isEmpty(close_time_2) && !isNumber(close_time_2)) {
-      errors.close_time_1 = res.__('RESTAURANT.close_time_1.valid');
+    if (!isEmpty(close_time_2)) {
+      if (!/^([0-1]?[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9])?$/.test(close_time_2)) {
+        errors.close_time_2 = res.__('RESTAURANT.close_time_1.invalid_time');
+      } else if (
+        close_time_2.replace(TIME_VALIDATION_REGEX, '$1$2$3') <= open_time_2.replace(TIME_VALIDATION_REGEX, '$1$2$3')
+      ) {
+        errors.close_time_2 = res.__('RESTAURANT.close_time_2.less_then_start_time');
+      }
     }
 
     if (Object.keys(errors).length > 0) {
